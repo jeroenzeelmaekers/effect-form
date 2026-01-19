02471128744050fdb0571d1427310ea5ad367e7e
Jeroen Zeelmaekers <mail@jeroenzeelmaekers.com>
2026-01-19 19:36:39 +0100

    feat: add posthog feat: add react compiler chore(deps): update deps

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,30 @@
 import EffectForm from './features/form';
 import UserList from './features/user-list';
 import { ThemeProvider } from './providers/theme-provider';
+import { PostHogProvider, useFeatureFlagEnabled } from 'posthog-js/react';
+
+const options = {
+  api_host: import.meta.env.VITE_PUBLIC_POSTHOG_HOST,
+  defaults: '2025-11-30',
+} as const;
 
 export default function App() {
+  const userCreationFlag = useFeatureFlagEnabled('allow-user-creation');
   return (
-    <ThemeProvider defaultTheme="system" storageKey="theme-preference">
-      <main className="flex min-h-screen flex-col items-baseline p-3 antialiased">
-        <h1 className="pl-3 text-2xl leading-loose font-semibold">
-          Effect Form Demo
-        </h1>
-        <div className="flex w-full flex-col gap-3 lg:flex-row">
-          <EffectForm />
-          <UserList />
-        </div>
-      </main>
-    </ThemeProvider>
+    <PostHogProvider
+      apiKey={import.meta.env.VITE_PUBLIC_POSTHOG_KEY}
+      options={options}>
+      <ThemeProvider defaultTheme="system" storageKey="theme-preference">
+        <main className="flex min-h-screen flex-col items-baseline p-3 antialiased">
+          <h1 className="pl-3 text-2xl leading-loose font-semibold">
+            Effect Form Demo
+          </h1>
+          <div className="flex w-full flex-col gap-3 lg:flex-row">
+            {userCreationFlag && <EffectForm />}
+            <UserList />
+          </div>
+        </main>
+      </ThemeProvider>
+    </PostHogProvider>
   );
 }
--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,14 @@ import { defineConfig } from 'vite';
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  plugins: [
+    react({
+      babel: {
+        plugins: [['babel-plugin-react-compiler']],
+      },
+    }),
+    tailwindcss(),
+  ],
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
@@ -20,6 +27,7 @@ export default defineConfig({
           form: ['@tanstack/react-form'],
           icons: ['lucide-react'],
           ui: ['@base-ui/react'],
+          posthog: ['posthog-js/react'],
         },
       },
     },
