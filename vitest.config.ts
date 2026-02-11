e0f75ee9fc8f072364946eca425679ebbe28e570
Jeroen Zeelmaekers <mail@jeroenzeelmaekers.com>
2026-02-11 22:29:45 +0100

    ci: add code coverage action

--- vitest.config.ts
+++ vitest.config.ts
@@ -6,6 +6,11 @@ export default defineConfig({
   test: {
     globals: true,
     environment: 'node',
+    coverage: {
+      provider: 'v8',
+      reporter: ['text', 'json', 'json-summary'],
+      reportsDirectory: 'coverage',
+    },
   },
   resolve: {
     alias: {
